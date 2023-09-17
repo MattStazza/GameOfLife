d3e9b14acf6accb404d40bc0e941906336a8e585
MattStazza <63791313+MattStazza@users.noreply.github.com>
2023-09-17 18:55:38 +0800

    Left & Right Buttons

--- loadUserInterface.lua
+++ loadUserInterface.lua
@@ -13,11 +13,18 @@ local function onLoadButtonTap(self)
     composer.gotoScene("gameScene", transitionOptions)
 end
 
-
 local function onCloseButtonTap(self)
     print("Close")
     local transitionOptions = { effect = "slideUp", time = 500, }
     composer.gotoScene("gameScene", transitionOptions)
+end
+
+local function onLeftButtonTap(self)
+    print("Left")
+end
+
+local function onRightButtonTap(self)
+    print("Right")
 end
 
 
@@ -49,17 +56,59 @@ function UI.createUI()
 
     -- CLOSE BUTTON -- 
    local closeButton = widget.newButton({
-    width = appData.buttonWidth  ,    
-    height = appData.buttonHeight,
-    label = "Close",
-    fontSize = 16,
-    labelColor = { default={1,1,1}, over={0.5,0.5,0.5} },
-    shape = "roundedRect",
-    fillColor = { default={1,0,0,1}, over={0.2,0.6,0.2,0.6} },
-    onRelease = function(event)
-        onCloseButtonTap(self)
-    end
-})
+        width = appData.buttonWidth  ,    
+        height = appData.buttonHeight,
+        label = "Close",
+        fontSize = 16,
+        labelColor = { default={1,1,1}, over={0.5,0.5,0.5} },
+        shape = "roundedRect",
+        fillColor = { default={1,0,0,1}, over={0.2,0.6,0.2,0.6} },
+        onRelease = function(event)
+            onCloseButtonTap(self)
+        end
+    })
+
+
+    -------------------------------------------------------------------
+
+    
+    -- LEFT BUTTON -- 
+    local leftIcon = display.newImage("leftIcon.png")
+    leftIcon.width = appData.buttonWidth / 4
+    leftIcon.height = appData.buttonWidth / 3
+
+    local leftButton = widget.newButton({
+        width = appData.buttonWidth / 4 ,    
+        height = appData.buttonHeight,
+        onRelease = function(event)
+            onLeftButtonTap(self)
+        end
+    })
+
+    leftIcon.x = leftButton.width / 2
+    leftIcon.y = leftButton.height / 2
+    leftButton:insert(leftIcon)
+
+
+
+    -- RIGHT BUTTON -- 
+    local rightIcon = display.newImage("rightIcon.png")
+    rightIcon.width = appData.buttonWidth  / 4
+    rightIcon.height = appData.buttonWidth  / 3
+
+    local rightButton = widget.newButton({
+        width = appData.buttonWidth / 4  ,    
+        height = appData.buttonHeight,
+        onRelease = function(event)
+            onRightButtonTap(self)
+        end
+    })
+
+    rightIcon.x = rightButton.width / 2
+    rightIcon.y = rightButton.height / 2
+    rightButton:insert(rightIcon)
+
+
 
     --===================================================================|
 
@@ -77,6 +126,12 @@ function UI.createUI()
     loadButton.x = display.contentWidth / 2
     loadButton.y = bottomY - (appData.buttonHeight * 2.5)
 
+    leftButton.x = 0 + (appData.buttonWidth / 4)
+    leftButton.y = display.contentHeight / 2
+    
+    rightButton.x = display.contentWidth - (appData.buttonWidth / 4)
+    rightButton.y = display.contentHeight / 2
+
     --------------------------------------------------------------------|
    
 
@@ -84,6 +139,8 @@ function UI.createUI()
     -- Insert Buttons into UIGroup
     uiGroup:insert(loadButton) 
     uiGroup:insert(closeButton)
+    uiGroup:insert(leftButton) 
+    uiGroup:insert(rightButton)
 
     return uiGroup
 
